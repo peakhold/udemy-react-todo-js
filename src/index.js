a7e2a388376eb93b49157757742a0da861d161c5
peakhold <99507613+peakhold@users.noreply.github.com>
2022-02-13 22:16:40 +0900

    link

--- src/index.js
+++ src/index.js
@@ -6,5 +6,5 @@ document.getElementById("app").innerHTML = `
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
+</div>  
 `;
